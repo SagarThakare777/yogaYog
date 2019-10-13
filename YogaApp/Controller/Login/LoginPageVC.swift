0678a57a8a804f7905e49519374e537c1836c817
SAGAR THAKARE <thakare.sagar26@gmail.com>
2019-10-13 16:26:33 +0530

    My Commits

---
+++ YogaApp/Controller/Login/LoginPageVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginPageVC.swift
+//  YogaApp
+//
+//  Created by SAGAR THAKARE on 24/08/19.
+//  Copyright © 2019 SAGAR THAKARE. All rights reserved.
+//
+
+import UIKit
+
+class LoginPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ YogaApp/Resources/BridgingHeader/YogaYog-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  YogaYog-Bridging-Header.h
+//  YogaApp
+//
+//  Created by SAGAR THAKARE on 25/08/19.
+//  Copyright © 2019 SAGAR THAKARE. All rights reserved.
+//
+
+#ifndef YogaYog_Bridging_Header_h
+#define YogaYog_Bridging_Header_h
+
+
+#endif /* YogaYog_Bridging_Header_h */
